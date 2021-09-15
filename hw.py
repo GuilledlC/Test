734e1b53cb089615fdd5bb1552437067d2af2892
Guillermo de la Cal Hacar <guillermodelacalhacar@gmail.com>
2021-09-15 03:09:47 -0700

    Update hw.py

--- hw.py
+++ hw.py
@@ -1,7 +1,7 @@
 print('Hello world! this is NOT a test')
 #a = int(input("Choose a number "))
 a = 2
-b = a + 1
-print("the NEXT number is ", b)
+b = a - 1
+print("the PREVIOUS number is ", b)
 #input()
 #comment
